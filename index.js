c55f638e1017003866c4cc345709f2769e38354b
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-07-12 20:16:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let multiply = num1 * num2;
 
-let random // 
+let random // Create a variable called random that will generate a random integer greater than 0.
+Create a variable called mod set to an equation whose remainder is equal to "4".
+Create a variable called max that will return "20" as the highest number in the set.
