030ce259025fcedc9c71a1e252533b11f269bb4c
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-07-12 20:25:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-function multiply(num1, num2) {
+let multiply(num1, num2) {
   return num1 * num2;
 }
 
 
-function random(){
-  return Math.floor(Math.random() * 10) + 1;
-}
+let random = Math.floor(Math.random() * 10) + 1;
 
-function mod(){
-  
-}
 let mod = 9 % 5;
 
 let max = Math.max(num1, num2);
